4aa2d02558cfe7f297776a9dd2af8f214b13e015
mabblesoft <mabblesoft@gmail.com>
2022-06-22 14:14:09 +0200

    crud-example

---
+++ src/constants/Routes.js
@@ -0,0 +1,13 @@
+const BLANK = '';
+
+export default {
+	BLANK,
+	GETSTARTED: 'GetStarted',
+	WELCOME: 'Welcome',
+	SIGNIN: 'SignIn',
+	SIGNUP: 'SignUp',
+	PROFILE: 'Profile',
+	SETTINGS: 'Settings',
+	DETAIL: 'Detail',
+	MODALTEMPLATE: 'ModalTemplate',
+};
---
+++ src/constants/index.js
@@ -0,0 +1,5 @@
+import Colors from './Colors.js';
+import Layout from './Layout.js';
+import Routes from './Routes.js';
+
+export { Colors, Layout, Routes };
